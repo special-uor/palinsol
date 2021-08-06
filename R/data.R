dc65d229aca8531259c5a5da4775f29fef8f4c61
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2021-08-07 00:04:07 +0100

    Add documentation for the datasets

---
+++ R/data.R
@@ -0,0 +1,75 @@
+#' Tables supplied by BER78
+#'
+#' Tables necessary for computation of astronomical solutions by
+#' \insertCite{berger1978long;textual}{palinsol}.
+#'
+#' @usage data(BER78)
+#'
+#' @format Text files reproducing the published tables with original units.
+#'
+#' @note Table 2 is reconstructed based on the electronic files supplied
+#' by the authors, based on the method  provided by
+#' \insertCite{berger1990origine;textual}{palinsol} below.
+#'
+#' @references
+#' \insertAllCited{}
+#'
+#' @source
+#' ftp://ftp.elic.ucl.ac.be/berger/berger78/
+#'
+#' @author
+#' Michel Crucifix, Université catholique de Louvain, Belgium.
+#'
+#' @examples
+#' data(BER78)
+"BER78"
+
+#' Tables supplied by BER90
+#'
+#' Tables necessary for computation of astronomical solutions by
+#' \insertCite{berger1990origine;textual}{palinsol}.
+#'
+#' @usage data(BER90)
+#'
+#' @format Text files reproducing the published tables with original units.
+#'
+#' @note Table 2 is reconstructed based on the electronic files supplied
+#' by the authors, based on the method  provided by
+#' \insertCite{berger1990origine;textual}{palinsol} below.
+#'
+#' @references
+#' \insertAllCited{}
+#'
+#' @source
+#' ftp://ftp.elic.ucl.ac.be/loutre/QSR/
+#'
+#' @author
+#' Michel Crucifix, Université catholique de Louvain, Belgium.
+#'
+#' @examples
+#' data(BER90)
+"BER90"
+
+#' Astronomical elements supplied by Laskar et al. 2004
+#'
+#' Astronomical elements (longitude of perihelion, obliquity and eccentricity)
+#' supplied by \insertCite{laskar2004long;textual}{palinsol} by step of 1ka,
+#' from -51Ma to present (la04past) and from present to + 21Ma.
+#'
+#' @usage data(LA04)
+#'
+#' @format Text files.
+#'
+#' @references
+#' \insertAllCited{}
+#'
+#' @source
+#' http://www.imcce.fr/Equipes/ASD/insola/earth/earth.html
+#'
+#' @author
+#' Michel Crucifix, Université catholique de Louvain, Belgium.
+#'
+#' @examples
+#' data(LA04)
+#'
+"LA04"
